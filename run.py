451340ea9dc9625ce929831568d7456da0812ef7
Jong Min <dlfnsdkcla@gmail.com>
2026-03-10 15:41:47 +0900

    최신 업데이트

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 from detect_v5 import extract_ancient_text
-ancient_text = extract_ancient_text('sample_images\옛한글.png')
-print(ancient_text)
+from translate_ocr import translate_image
+
+ancien_text = extract_ancient_text('sample_images\옛한글.png')
+
+result = translate_image(
+      "sample_images/옛한글.png",
+      ocr_text= ancien_text
+  )
+
+print(result)
